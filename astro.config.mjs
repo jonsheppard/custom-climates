75cda1867e4181518589ddb345df250b1dec1020
Jon Sheppard <jonsheppard@users.noreply.github.com>
2025-06-20 22:11:44 -0600

    fix(deps): Correct tailwind setup and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://customclimates.netlify.app',
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [sitemap()]
+  integrations: [tailwind(), sitemap()]
 });
